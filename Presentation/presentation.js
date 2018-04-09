ad981ed104dfd40ac81be9b82c9767a10ec5c9e8
andrew.everall1995@gmail.com <andy@andy-Lenovo-YOGA-510-14ISK>
2018-04-09 19:01:05 +0100

    removed unneeded files from repository

--- Presentation/presentation.js
+++
@@ -1,9 +0,0 @@
-//Start file when the HTML is ready
-$(document).ready(function() { //Open backets for ready function
-
-$(".rave-fields").click( function() {
-	$(this).css("width", "50%")
-	
-})
-
-}); //Close brackets for ready() function
