b575e6bf4805d016f3cfb68ee2d59f797b3efc0b
andrew.everall1995@gmail.com <andrew.everall1995@gmail.com>
2018-07-30 14:11:47 +0100

    Edit setup. Add version to __init__.py

--- seestar/__init__.py
+++ seestar/__init__.py
@@ -2,3 +2,9 @@ __all__ = ['IsochroneScaling', 'ArrayMechanics', 'StatisticalModels',
  			'createNew', 'setdatalocation', 'DistributionPlots', 
  			'FieldUnions', 'SelectionGrid', 'FieldAssignment'
  			'SFInstanceClasses', 'surveyInfoPickler']
+
+from sys import version as pyversion
+if pyversion > '3': # Load in Python 3 version
+    __version__ = 'v1.3.5-c'
+elif (pyversion<'3') & (pyversion>'2.7'): # Load in Python 2.7 version
+    __version__ = 'v1.2.7-c'
